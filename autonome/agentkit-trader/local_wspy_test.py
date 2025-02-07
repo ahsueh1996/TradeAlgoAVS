7bbba5dff4c17a33cd7f5c6a907afa9951a3a569
ahsueh1996 <ahsueh1996@gmail.com>
2025-02-07 17:46:40 -0500

    add local test

---
+++ autonome/agentkit-trader/local_wspy_test.py
@@ -0,0 +1,19 @@
+from wspy import Client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+WS_EMAIL = os.environ.get("WS_EMAIL")
+WS_PASSWORD = os.environ.get("WS_PASSWORD")
+
+client = Client()
+while client.login_status != "OK":
+    action = input("Action: ")
+    if action == "login":
+        client.login(WS_EMAIL, WS_PASSWORD)
+    elif len(action) == 6 and action.isnumeric():
+        client.otp(action)
+    print("Login Status: "+client.login_status)
+
+print("End.")
