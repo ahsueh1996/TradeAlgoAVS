d240816c63f7e43d94795ad8ac5cd4b1937c1183
ahsueh1996 <ahsueh1996@gmail.com>
2025-02-07 21:16:54 -0500

    Update __init__.py

---
+++ autonome/agentkit-trader/cdp-agentkit-core/cdp_agentkit_core/actions/finance/wealthsimple/__init__.py
@@ -0,0 +1,26 @@
+# Core Action Class
+from cdp_agentkit_core.actions.finance.wealthsimple.action import WealthsimpleAction
+# =============================== Register all other Actions below ===============================================
+#from cdp_agentkit_core.actions.finance.wealthsimple.xxx import yyyAction
+
+
+def get_all_wealthsimple_actions() -> list[type[WealthsimpleAction]]:
+    actions = []
+    for action in WealthsimpleAction.__subclasses__():
+        actions.append(action())
+
+    return actions
+
+
+WEALTHSIMPLE_ACTIONS = get_all_wealthsimple_actions()
+
+
+# Manually List out all the actions.. 
+__all__ = [
+    "WEALTHSIMPLE_ACTIONS",
+    "AccountDetailsAction",
+    "AccountMentionsAction",
+    "PostTweetAction",
+    "PostTweetReplyAction",
+    "TwitterAction",
+]
