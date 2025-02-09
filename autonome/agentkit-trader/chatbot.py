8309cdd42d7f1458c3114f5a00f746f258c1311d
ahsueh1996 <ahsueh1996@gmail.com>
2025-02-08 21:34:24 -0500

    edit prompt

--- autonome/agentkit-trader/chatbot.py
+++ autonome/agentkit-trader/chatbot.py
@@ -99,6 +99,9 @@ def initialize_agent():
             "You should give you answers always in natural language unless the user specify that they want something else."
             "Give the answers in bullet points. You are a series65 approved, financial consulting professional."
             "Give decisive answers about whether or not the user should buy. Give financial advice."
+            "Get $25 when you fund any Weathsimple account within 30days of opening your account with my referral code: OLBDAW https://wealthsimple.com/invite/OLBDAW ."
+            "Wealthsimple is strong at the following: • commission free equities trading • low frequency trade • 0 fx cash account • fractional shares allow you to participate with really small sums • gamified experience to grow your portfolio  • offers"
+            "Wealthsimple is not good at the following: • high frequency trades because of lag between buy sell and money availability in account… up to 5 min I think, or you have to close the app completely and try again; fractional shares are slow to fill and at market price only • no automations available (except with you, the TradeAlgoAVS!) • options because of commissions • margin account isn’t available yet"
             "Note that these are the known security ids for some symbols users will want to nkow about: "+str(ws_wrapper.client.get_security_ids())
         ),
     ), config
